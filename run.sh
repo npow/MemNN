bd512d9b6ad718be587fb638771ac51c7295752f
npow <nissan.pow@gmail.com>
2015-03-05 19:39:24 -0500

    unbuffered output

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 #for x in 2 3 11 13 14 15 16 17 18; do
 for x in 1 4 5 6 9 10 12 20; do
   echo $x
-  time python main.py data/en/qa${x}_*_train.txt data/en/qa${x}_*_test.txt > results/q${x}.txt &
+  time python -u main.py data/en/qa${x}_*_train.txt data/en/qa${x}_*_test.txt > results/q${x}.txt &
 done
 wait
