7bd85b249e56f6a4fc30a96869deb45b83b4f2ab
npow <nissan.pow@gmail.com>
2015-06-15 13:29:02 -0400

    update scripts

--- test_gamma.sh
+++ test_gamma.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for gamma in 0.1 0.01 0.001 1 10 100; do
   echo $gamma
-  python -u main.py data/en/qa2_*train.txt data/en/qa2_*test.txt $gamma > results/q2_${gamma}.txt &
+  python -u main.py --train_file data/en/qa2_*train.txt --test_file data/en/qa2_*test.txt --gamma $gamma > results/q2_${gamma}.txt &
 done 
 wait
