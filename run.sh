d0f0ba9eb8a32356a8d7ae18c18c9b10dd93c040
npow <nissan.pow@gmail.com>
2015-06-15 16:22:30 -0400

    add sleep to run script

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-for x in 2 3 11 13 14 15 16 17 18; do
-#for x in 1 4 5 6 9 10 12 20; do
+export THEANO_FLAGS=device=cpu
+
+#for x in 2 3 11 13 14 15 16 17 18; do
+for x in 1 4 5 6 9 10 12 20; do
   echo $x
   time python -u main.py --train_file data/en/qa${x}_*_train.txt --test_file data/en/qa${x}_*_test.txt > results/q${x}.txt &
+  sleep 1
 done
 wait
