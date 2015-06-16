8ebc84e304064984e9a6ca4b539a2ff5891af111
npow <nissan.pow@gmail.com>
2015-06-15 22:36:03 -0400

    add gridsearch

--- test_gamma.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for gamma in 0.1 0.01 0.001 1 10 100; do
-  echo $gamma
-  python -u main.py --train_file data/en/qa2_*train.txt --test_file data/en/qa2_*test.txt --gamma $gamma > results/q2_${gamma}.txt &
-done 
-wait
